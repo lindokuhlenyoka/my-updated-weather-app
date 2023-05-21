f5c51c1409cf7ce4eaadb160146a729aaabaa42f
lindokuhlenyoka <106947324+lindokuhlenyoka@users.noreply.github.com>
2023-05-21 15:30:14 +0200

    Changed API key and function names
    Typing error caused the function to not get recognised.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function formateDate(timestamp) {
+function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "0fc86fba2b8c92606f56e444df1a4b7a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiKey = "0fc86fba2b8c92606f56e444df1a4b7a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
